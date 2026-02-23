24073cffcb95b34386495171103a262d533baac1
motlamelle <lebomotlamelle@gmail.com>
2026-02-23 13:20:53 +0200

    black formatter used

---
+++ fse_2.py
@@ -0,0 +1,17 @@
+# define list
+age_list = [21, 22, 23, 34]
+
+# define function
+
+
+def print_list(list_arg):
+
+    for age in list_arg:
+        print(age)
+
+
+def is_unique(list_arg):
+
+    if __name__ == "__main__":
+
+        print_list(age_list)
